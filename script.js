bc44f2529895602d81f90250b7da1b5df3c5ae63
shelldan <shelldanlee@gmail.com>
2022-09-12 19:16:44 -0500

    update

--- script.js
+++ script.js
@@ -1,26 +1,148 @@
 //Erica's Task - creating .fetch to spoonacular API
 
-//List of Parameters
-var cuisine;
-var protein; //? Not sure about how to add this to API on line 11
+//Spoonacular API documentation: https://spoonacular.com/food-api/docs
+//Spoonacular Authentication: https://spoonacular.com/food-api/docs#Authentication
+var page2Div = document.createElement('div')
+var page3Div = document.createElement('div')
+var page4Div = document.createElement('div')
 
-//Spoonacular API Information
-var spoonUrl = 'https://api.spoonacular.com/recipes/complexSearch?query=' + protein + '&cuisine=' + cuisine;
-var spoonApiKey = 'bff7143ee08c4a6aa8d53b4a91fc839f'
+var page1Div = document.querySelector('.page-1')
 
-//searchSpoon function fetches response from Spoonacular API
-var searchSpoon = function () {
-  fetch(spoonUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
+var cuisineBtn = document.getElementById('cuisine')
+var mainDiv = document.querySelector('main')
 
-      return response.json();
+var frenchStyle = document.getElementById('french')
+var spoonApiKey = 'c95dcc6d89ab47a384f18b2f989d7286'
+
+var ids = [] //might need to use array to remove duplicate id
+//var recipeInfoUrl = 'https://api.spoonacular.com/recipes/' + id +'/information'
+
+
+
+function page2handler (event){
+
+    page1Div.style.display = 'none'
+
+    page2Div.setAttribute('class','page-2')
+
+    var french = document.createElement('button')
+    var american = document.createElement('button')
+    var german = document.createElement('button')
+    var italian = document.createElement('button')
+
+    mainDiv.appendChild(page2Div)
+    page2Div.appendChild(french)
+    page2Div.appendChild(american)
+    page2Div.appendChild(german)
+    page2Div.appendChild(italian)
+
+    french.innerHTML = 'French'
+    american.innerHTML = 'American'
+    german.textContent = 'German'
+    italian.textContent = 'Italian'
+
+    console.log(event.target.textContent) //cuisine
+
+    $("button").click(page3handler) //stackoverflow: how to get ID of button user just clicked? 
+    //$("button").click(getRecipeName)
+}
+
+
+function page3handler(event){
+
+    page2Div.style.display = 'none';
+
+    page3Div.setAttribute('class','page-3')
+
+
+    console.log(event.target.textContent) // French 
+
+    //cuisineHeader.textContent = 'Bake Ratatouille' //for testing, will delete later 
+
+    var cuisine = event.target.textContent
+    
+    var cuisineUrl = 'https://api.spoonacular.com/recipes/complexSearch?' +'&cuisine=' + cuisine + '&apiKey=' + spoonApiKey
+    
+    console.log(cuisineUrl)
+
+    function getRecipeName(){
+        fetch(cuisineUrl)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(recipeName){
+            console.log(recipeName)
+            
+
+            for (var i=0; i<10; i++){ //limited to 10 per page, butwhy can't use i<recipeName.length? 
+                var cuisineHeader = document.createElement('h2')
+                var cuisineImg = document.createElement('img')
+                var cuisineRecipe = document.createElement('p') // not sure if we want to add here
+
+                cuisineHeader.textContent = recipeName.results[i].title
+                cuisineImg.src=recipeName.results[i].image
+                cuisineHeader.setAttribute('id',recipeName.results[i].id)
+
+                cuisineHeader.addEventListener('click',function(){
+                    console.log(this)
+                    var id = this.getAttribute('id')
+
+                    console.log(id)
+                    page4handler(id)
+                })
+
+                mainDiv.appendChild(page3Div)
+                page3Div.appendChild(cuisineHeader)
+                page3Div.appendChild(cuisineImg)
+                page3Div.appendChild(cuisineRecipe)
+
+            }
+
+           // cuisineHeader.addEventListener('click',page4handler)
+            
+            
+        })
+    }
+    getRecipeName()
+}
+
+
+function page4handler(id){
+    page2Div.style.display= 'none';
+    page3Div.style.display= 'none';
+
+    page4Div.setAttribute('class','page-4')
+
+    var cuisineHeader = document.createElement('h2')
+    var cuisineImg = document.createElement('img')
+    var cuisineRecipe = document.createElement('p')
+
+    mainDiv.appendChild(page4Div)
+    page4Div.appendChild(cuisineHeader)
+    page4Div.appendChild(cuisineImg)
+    page4Div.appendChild(cuisineRecipe)
+
+    //var id = localStorage.getItem('id')
+
+    var recipeInfoUrl = 'https://api.spoonacular.com/recipes/' + id +'/information?' + '&apiKey=' + spoonApiKey
+
+    fetch(recipeInfoUrl)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(recipeInfo){
+        //console.log(recipeInfo)
+        cuisineHeader.textContent = recipeInfo.title
+        cuisineImg.src=recipeInfo.image
+        cuisineRecipe.innerHTML=recipeInfo.summary
     })
 
-    .then(function (response) {
-      console.log('Response is good!');
-      console.log(response);
-    });
-};
+}
+
+
+cuisineBtn.addEventListener('click',page2handler)
+
+
+
+
+
